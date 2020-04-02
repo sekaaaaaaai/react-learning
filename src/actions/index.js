9f280b50f62c2c96bc295f8c035124276ddd137d
Seiji Sakai <iruru147@icloud.com>
2020-04-03 06:58:31 +0900

    add action

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'INCREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+export const increment = () => ({
+  type: DECREMENT
+})
