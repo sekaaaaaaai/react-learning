a689790662e15a95480beda9a8596ec55caa0a07
Sekaaaaaaai <sakai.c1rcu1tsky@gmail.com>
2020-04-05 15:25:49 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+import axios from 'axios'
 
-export const increment = () => ({
-  type: INCREMENT
-})
-export const decrement = () => ({
-  type: DECREMENT
-})
+export const READ_EVENTS = 'READ_EVENTS'
+
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
+const QUERY_STRING = '?token=token123';
+
+export const readEvents = () => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events${QUERY_STRING}`);
+  dispatch({ type: READ_EVENTS, response })
+}
